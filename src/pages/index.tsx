fdf6233eb3353f9310d163c1d1982a9bd1636973
Buletiss <vinicius1997feitosa@gmail.com>
2022-11-06 05:50:57 -0300

    feat: :sparkles: adição da pagina home, consumo da api e mostrando nomes em tela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,40 @@
+import { Flex, Box, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import { Navbar } from '../components/NavBar';
+import api from '../services/api';
+
+interface Character {
+  name: string;
+  gender: string;
+}
 
 export default function Home() {
-  return <Navbar />;
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  useEffect(() => {
+    async function getPersonInfor() {
+      const response = await api.get('/people');
+      setCharacters(response.data.results);
+    }
+    getPersonInfor();
+  }, []);
+  return (
+    <>
+      <Flex flexDirection="column" backgroundImage="url(/spaceStar.png)">
+        <Navbar />
+        <Flex justifyContent="center">
+          <Flex flexDirection="row" flexWrap="wrap" w="80%">
+            {characters.map((character, index) => (
+              <Flex key={index} w="20%" margin="35px 25px 20px 25px">
+                <Box w="100%" h="350px" border="1px" borderColor="white" color="white">
+                  <Text>{character.name}</Text>
+                  <Text>{character.gender}</Text>
+                </Box>
+              </Flex>
+            ))}
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
 }
